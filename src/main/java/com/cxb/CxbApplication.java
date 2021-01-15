2f7cf1797c14804f54bbcff7ff25bd4e871c5fcc
cxb2468 <cxb2468@qq.com>
2021-01-15 17:14:15 +0800

    mapper接口 与 mapper.xml 无法识别绑定 ?疑难问题  !

--- src/main/java/com/cxb/CxbApplication.java
+++ src/main/java/com/cxb/CxbApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.cxb.mapper")
+
 public class CxbApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/cxb/mapper/GoodsMapper.java
+++ src/main/java/com/cxb/mapper/GoodsMapper.java
@@ -2,6 +2,7 @@ package com.cxb.mapper;
 
 import com.cxb.entity.Goods;
 import com.github.pagehelper.Page;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ import java.util.List;
  * @Author Cxb
  * @Date 2021-01-15 15:48
  */
+@Mapper
 public interface GoodsMapper {
     //CRUD
 
